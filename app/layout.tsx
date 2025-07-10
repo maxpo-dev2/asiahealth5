a711c03fd0057c34b93e2c5e1e0379819103491f
Sandeep1816 <sandeepavala2@gmail.com>
2025-07-10 12:41:42 +0530

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import Footer from "./components/footer";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"], 
- variable: "--font-dm-sans",
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-dm-sans",
   display: "swap",
 });
 
@@ -18,19 +18,63 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
     shortcut: "/logo/favicon-32x32.png",
     apple: "/logo/apple-touch-icon.png",
-}
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-       className={`${dmSans.variable} font-sans antialiased`}
-      >
+      <head>
+        {/* ✅ Google Site Verification for GSC */}
+        <meta
+          name="google-site-verification"
+          content="J7DDA16rBH1POOjOrzSgH77ba07DNWOQC2uBmeXCnAY"
+        />
+
+        {/* ✅ Google Analytics Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HXNXK909ES"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HXNXK909ES');
+            `,
+          }}
+        />
+
+        {/* ✅ Google Tag Manager (GTM) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K69L6F43');
+            `,
+          }}
+        />
+      </head>
+      <body className={`${dmSans.variable} font-sans antialiased`}>
+        {/* ✅ GTM NoScript immediately after <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K69L6F43"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         {children}
         <Footer />
